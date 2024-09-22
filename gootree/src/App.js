1c754b79026c59c75bf9c2169b3908e7f5ecc076
jasleena-av <jasleenansar006@gmail.com>
2024-09-22 15:34:36 +0530

    signup-ui

--- gootree/src/App.js
+++ gootree/src/App.js
@@ -1,13 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import Signup from './pages/Signup';
+
 
 function App() {
   return (
     <div >
       
       <Routes>
+        
         <Route path='/login' element={<Login/>} />
+        <Route path='/sign-up' element={<Signup/>} />
+
       </Routes>
     </div>
   );
